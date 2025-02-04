18739d5020632ce0dcde5dc6b9042f632fc16cb1
Daniel Spilchuk <daniel.spilchuk@esti.ca>
2025-02-04 12:59:38 -0600

    Able to change music on start. Begin adding options.

--- Connection.py
+++ Connection.py
@@ -1,35 +1,46 @@
 import spotipy
+import json
 from spotipy.oauth2 import SpotifyOAuth
 from Credentials import *
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID,
                                                client_secret=SPOTIPY_CLIENT_SECRET,
                                                redirect_uri=SPOTIPY_REDIRECT_URI,
-                                               scope="user-library-read"))
+                                               scope="user-read-playback-state,user-modify-playback-state"))
+
+#https://api.spotify.com/v1/me/player/devices get request to find all
 
 # base information to be changed in the future
-device_id = 90
-volume_percent = 20
+device_id = None
+volume_percent = 80
+
+
+#context_uri="spotify:playlist:3qu09J0oigKWBhPWoRJsjs"
+search_return = sp.search("peeing in a pool while it rains", limit=1, type='playlist')
+json_parse = json.loads(search_return)
+context_uri = json_parse['playlists']['items']['uri']
+
+# search # searches for an item playlist(playlist_id, fields=None, market=None, additional_types=('track',))
+
+print('INTRO')
+print(sp.devices()) # how to do this more specifically
+print(context_uri)
 
 # Basic Features/Goals
 # Get User Inputs for Key and Playlist
 # Play Playlist on Key Press (unless already playing?)
 
 # METHODS
-# search # searches for an item playlist(playlist_id, fields=None, market=None, additional_types=('track',))
-#playlist(playlist_id, fields=None, market=None, additional_types=('track',)) get playlist by id
 #print(sp.currently_playing()) # users currently playing track
 #print(sp.current_playback()) # get information about current users currently playing track
-# user_playlist(user, playlist_id=None, fields=None, market=None)
 
+print('Changing Volume..')
 sp.volume(volume_percent, device_id)
-sp.start_playback(device_id, context_uri=None, uris=None, offset=None, position_ms=None)
+
+print('Starting Playback..')
+sp.start_playback(device_id, context_uri, uris=None, offset=None, position_ms=None)
+
+print('Shuffling Music..')
 sp.shuffle(True, device_id)
 
-#https://open.spotify.com/playlist/3qu09J0oigKWBhPWoRJsjs?si=NMX6BIKeQ5Ob1GhQVaawfQ
-
-# generic code for  
-results = sp.current_user_saved_tracks() # liked songs
-for idx, item in enumerate(results['items']):
-    track = item['track']
-    print(idx, track['artists'][0]['name'], " - ", track['name'])
+print("Process Completed.")
