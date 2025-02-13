06178dc435792cd21af60cc4c18438af5bbaf4f2
Daniel Spilchuk <daniel.spilchuk@esti.ca>
2025-02-13 13:14:00 -0600

    Add further error handling. Allows for future development for programmatic opening.

--- Connection.py
+++ Connection.py
@@ -1,7 +1,31 @@
 import sys
+from dotenv import load_dotenv
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-from Credentials import *
+import os
+
+# New rough authorization?? Different language snippet
+#authUrl = new URL("https://accounts.spotify.com/authorize")
+# generated in the previous step
+#window.localStorage.setItem('code_verifier', codeVerifier);
+
+#params =  {
+#  response_type: 'code',
+#  client_id: clientId,
+#  scope,
+#  code_challenge_method: 'S256',
+#  code_challenge: codeChallenge,
+#  redirect_uri: redirectUri
+#}
+#authUrl.search = new URLSearchParams(params).toString();
+#window.location.href = authUrl.toString();
+
+load_dotenv()
+SPOTIPY_CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
+SPOTIPY_CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
+SPOTIPY_REDIRECT_URI = os.getenv("SPOTIPY_REDIRECT_URI")
+
+print(SPOTIPY_CLIENT_ID)
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID,
                                                client_secret=SPOTIPY_CLIENT_SECRET,
@@ -10,10 +34,18 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID,
 
 #https://api.spotify.com/v1/me/player/devices get request to find all
 
-# error handling for no active device player
+# THIS SECTION IS TO BE ADDED TO. functionality options- open spotify programmatically?
+# error handling for no open spotify
 if len(sp.devices()['devices']) == 0:
-    print('No Active Device Running Spotify! Please listen to music and try again.')
+    print('No Devices With Spotify Open! Please start the app and try again.')
     sys.exit()
+
+# error handling for no active spotify devices
+elif len(sp.devices()['devices']) > 0:
+    for i in sp.devices()['devices']:
+        if not i['is_active']:
+            print('No Active Devices Running Spotify! Please listen to music and try again.')
+            sys.exit()
 
 # base information to be changed in the future
 device_id = None
