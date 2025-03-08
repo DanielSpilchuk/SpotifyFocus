eac6376aaadcf5fec87a5e7a5ccc0c01a43e24aa
Daniel Spilchuk <daniel.spilchuk@esti.ca>
2025-03-07 19:27:03 -0600

    Add automatic play/pause to process.

--- Connection.py
+++ Connection.py
@@ -26,6 +26,26 @@ import time
 #authUrl.search = new URLSearchParams(params).toString();
 #window.location.href = authUrl.toString();
 
+def activateSpotifyPlayer():
+    import pyautogui
+    import pygetwindow as gw
+
+    # pip install pyautogui
+    # pip install pygetwindow
+    # assist in development????
+
+    # retrieve spotify window by title, activate, then press space
+    window = gw.getWindowsWithTitle("Spotify Premium")
+    try:
+        win = window[0]
+        win.activate() 
+        pyautogui.press("space")
+    except:
+        print("Window not found!")
+
+    time.sleep(1.5)
+
+
 # find the issue of user key storage, how to request and recieve
 load_dotenv()
 SPOTIPY_CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
@@ -39,48 +59,58 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID,
 
 #https://api.spotify.com/v1/me/player/devices get request to find all
 
+spotifyDevices = sp.devices()['devices']
+
 # attempt to open spotify programmatically on local machine if no devices are available
-if len(sp.devices()['devices']) == 0:
+if len(spotifyDevices) == 0:
     print('No Devices With Spotify Open! Attempting to open it for you.')
 
-    # application solution (semi-finished currently)
-    # i personally cannot believe that this works
-    # error handle if not successful
+    # application solution. confirmed solution on windows machines.
     try:
         os.system("spotify")
-        startupGracePeriod = 5
+        startupGracePeriod = 6 
     except:
-        # web solution (fixes this step of the process)
+        # web solution if windows application fails
         webbrowser.open("https://open.spotify.com/")
         startupGracePeriod = 15
 
-    # give time to run
+    # give time to run based on application time
     time.sleep(startupGracePeriod)
     
 # error handling for no active spotify devices
-if len(sp.devices()['devices']) > 0:
+if len(spotifyDevices) > 0:
+ 
+    # sometimes it thinks something is playing when nothing is???
+    # ah shit it can sense my smartwatch, can't believe tech these days
+    #['is_active': False, 
+    # 'is_private_session': False, 
+    # 'is_restricted': False, 
+    # 'name': 'SM-R940', 
+    # 'supports_volume': False, 
+    # 'type': 'Smartwatch', 
+    # 'volume_percent': 100}]
+    print(spotifyDevices)
 
     # learn how to set a device to be active
     # play on startup
     # app or web better?
     # need of an external server potentially
     
-    #anyDevicesActive = False
-    #for i in sp.devices()['devices']:        
-    #    if i['is_active']:
-    #        anyDevicesActive = True
-    
-
     # can we falsify a play/pause key press
     # hmmmmmmmmmmm
-    # sounds neato 
+    # sounds neato. note- yup its neato
+    
 
     # as long as one device is active then we can run playback
-    anyDevicesActive = any(i['is_active'] for i in sp.devices()['devices'])
+    anyDevicesActive = any(i['is_active'] for i in spotifyDevices)
     
     if not anyDevicesActive:
-        print('No Active Devices Running Spotify! Please listen to music and try again.')
-        sys.exit()
+        print('No Active Devices Running Spotify! Attempting to activate it for you.')
+           
+        activateSpotifyPlayer()
+        
+        # give program time before attempting searches
+        time.sleep(0.5)
 
 # base information to be changed in the future
 device_id = None
@@ -99,7 +129,11 @@ context_uri = search_return['playlists']['items'][0]['uri']
 
 # volume modification
 print('Changing Volume..')
-sp.volume(volume_percent, device_id)
+try:
+    sp.volume(volume_percent, device_id)
+except:
+    activateSpotifyPlayer()
+    sp.volume(volume_percent, device_id)
 
 # starting playback based on searched field
 print('Starting Playback..')
